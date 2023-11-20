95f482798c090394894cc999b6c9b0291999259b
erika.westman <erika.westman@elev.ga.ntig.se>
2023-11-20 15:05:23 +0100

    lektion 3

---
+++ VinterprojProg2/Obstacle.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Numerics;
+namespace VinterprojProg2;
+using Raylib_cs;
+
+
+public class Obstacle
+{
+    Variables v = new Variables();
+    public int obstacleX = Raylib.GetScreenWidth();
+    public static int maxObstacles = 4;
+    public bool nextObstacle;
+    public int obstacleSpace;
+    public int speed = 10;
+    public Random generator = new Random();
+    public int heightGenHigh;
+    public int heightGenLow;
+    
+    public Rectangle getPipeL()
+    {
+        return new Rectangle(obstacleX, heightGenLow, 64, 800);
+    }
+    public Rectangle getPipeH()
+    {
+        return new Rectangle(obstacleX, heightGenHigh, 64, 800);
+    }
+    public Obstacle()
+    {
+        heightGenHigh = generator.Next(-800, Raylib.GetScreenHeight() - 1200);
+        heightGenLow = heightGenHigh + 1200;
+    }
+    public void DrawObstacle()
+    {   
+        Raylib.DrawRectangleRec(getPipeH(), Color.GREEN);
+        Raylib.DrawRectangleRec(getPipeL(), Color.GREEN);
+        Console.WriteLine(obstacleX);
+    }
+    public void MoveObstacle()
+    {
+        obstacleX = obstacleX - speed;
+    }
+    public void Space()
+    {
+        obstacleSpace += speed;
+    }
+    
+
+}
---
+++ VinterprojProg2/Program.cs
@@ -0,0 +1,88 @@
+﻿// något liknande flappy bird
+
+using Raylib_cs;
+using System.IO.Pipes;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using VinterprojProg2;
+
+Raylib.InitWindow(1920, 1080, "Flappy Bird");
+Raylib.SetTargetFPS(30);
+
+Player bird = new Player();
+Variables v = new Variables();
+List<Obstacle> obstacles = new();
+
+while(Raylib.WindowShouldClose() == false)
+{
+
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.SKYBLUE);
+    Console.WriteLine(obstacles);
+
+    bird.Flap();
+    bird.DrawCharacter();
+    bird.IsDead(); 
+
+    if (obstacles.Count <= Obstacle.maxObstacles)
+    {   
+        if (obstacles.Count < 1 ||  > Raylib.GetScreenWidth() / Obstacle.maxObstacles)
+        {
+            obstacles.Add(new Obstacle());
+            if ( > Raylib.GetScreenWidth() / Obstacle.maxObstacles)
+            {
+                pipe.obstacleSpace = 0;
+            }
+        }
+       
+    }
+
+    foreach(Obstacle i in obstacles)
+    {
+        Console.WriteLine(i.obstacleX);
+        i.DrawObstacle();
+        i.MoveObstacle();
+    }
+
+    if (obstacles.Count > pipe.maxObstacles)
+    {
+        obstacles.RemoveAt(0);
+    }
+
+    bool isColliding()
+    {
+        foreach(Obstacle n in obstacles)
+        {
+            Raylib.CheckCollisionRecs(bird.getRect(), pipe.getPipeH());
+            Raylib.CheckCollisionRecs(bird.getRect(), pipe.getPipeL());
+            if (Raylib.CheckCollisionRecs(bird.getRect(), pipe.getPipeH()) || Raylib.CheckCollisionRecs(bird.getRect(), pipe.getPipeL()))
+            {
+                return true;
+            }
+            else if (bird.charPosY > Raylib.GetScreenHeight() - 64)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+
+    if(isColliding())
+    {
+        bird.dead = true;
+    }
+    
+    Raylib.DrawRectangle((int)obstacles[0].getPipeH().X, (int)obstacles[0].getPipeH().Y, (int)obstacles[0].getPipeH().Width, (int)obstacles[0].getPipeH().Height, Color.WHITE);
+    Console.WriteLine(pipe.getPipeH());
+    Raylib.DrawRectangle((int)bird.getRect().X, (int)bird.getRect().Y, (int)bird.getRect().Width, (int)bird.getRect().Height, Color.WHITE);
+    
+    Raylib.EndDrawing();
+    
+}
+
+
